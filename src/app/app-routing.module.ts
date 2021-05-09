aca329666e0daa116a50447fa803030416bf19a7
Rahul <rahulsirwani181989@gmail.com>
2021-05-09 22:15:40 +0530

    Project Shree Ganesh

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  {path : 'home-page' , component: HomePageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomePageComponent]
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'angularDemo1';
+  showHomePage = true;
+
+ngOnInit()
+{
+  this.showHome();
+}
+
+  showHome()
+  {
+    return this.showHomePage = !this.showHomePage;
+  }
+}
