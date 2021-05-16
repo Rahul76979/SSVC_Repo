dfe07272b3c3f30acc6bef96336f8fc86b5d0757
divik09 <diviktwr4@gmail.com>
2021-05-16 17:51:31 +0530

    added aboutus contactus and gallery components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  {path : 'home-page' , component: HomePageComponent}
+  { path : '' , component: AppComponent },
+  { path: 'aboutUs' ,component: AboutUsComponent},
+  { path: 'homePage', component: HomePageComponent},
+  { path: 'contactUs', component: ContactUsComponent}
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
 export const routingComponents = [HomePageComponent]
